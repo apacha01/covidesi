e99803b5efcef046bd2789a8f6e9b4e2491f6c69
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-22 11:57:11 -0300

    Refactor contrast functions onto utils file

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,46 @@ export const hex2rgb = (hex) => {
 export const rgb2hex = ({ r, g, b }) => {
 	return '#' + r.toString(16).padStart(2, 0) + g.toString(16).padStart(2, 0) + b.toString(16).padStart(2, 0);
 }
+
+// Calculate relative luminance with w3 (https://www.w3.org/WAI/GL/wiki/Relative_luminance) formula:
+// L = 0.2126 * R + 0.7152 * G + 0.0722 * B where R, G and B are defined as:
+// if RsRGB <= 0.04045 then R = RsRGB/12.92 else R = ((RsRGB+0.055)/1.055) ^ 2.4
+// if GsRGB <= 0.04045 then G = GsRGB/12.92 else G = ((GsRGB+0.055)/1.055) ^ 2.4
+// if BsRGB <= 0.04045 then B = BsRGB/12.92 else B = ((BsRGB+0.055)/1.055) ^ 2.4
+// and RsRGB, GsRGB, and BsRGB are defined as:
+// RsRGB = R8bit/255
+// GsRGB = G8bit/255
+// BsRGB = B8bit/255
+export const calculateRelativeLuminance = ({ r, g, b }) => {
+	let R, G, B;
+
+	let rs = r / 255;
+	let gs = g / 255;
+	let bs = b / 255;
+
+	if (rs <= 0.04045) R = rs / 12.92;
+	else R = Math.pow((rs + 0.055) / 1.055, 2.4);
+
+	if (gs <= 0.04045) G = gs / 12.92;
+	else G = Math.pow((gs + 0.055) / 1.055, 2.4);
+
+	if (bs <= 0.04045) B = bs / 12.92;
+	else B = Math.pow((bs + 0.055) / 1.055, 2.4);
+
+	return 0.2126 * R + 0.7152 * G + 0.0722 * B;
+};
+
+// Calculate contrast with w3 (https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html#key-terms) formula:
+// contrast ratio
+// (L1 + 0.05) / (L2 + 0.05), where
+// 		L1 is the relative luminance of the lighter of the colors, and
+// 		L2 is the relative luminance of the darker of the colors.
+export const calculateContrastRatio = (hexColorString1, hexColorString2) => {
+	const L1 = calculateRelativeLuminance(hex2rgb(hexColorString1));
+	const L2 = calculateRelativeLuminance(hex2rgb(hexColorString2));
+
+	// L1 is darker
+	if (L1 < L2) return (L2 + 0.05) / (L1 + 0.05);
+	// L1 is lighter
+	else return (L1 + 0.05) / (L2 + 0.05);
+};
