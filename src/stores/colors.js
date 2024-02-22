19ed5b722ded53c874c2e38a264c0073c685c8ba
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-22 08:42:58 -0300

    Refactor color inputs to now update the colors store. Adapt contrast checker to accept 3 digit hex strings. Implement setters for colors store.

--- src/stores/colors.js
+++ src/stores/colors.js
@@ -1,4 +1,17 @@
 import { atom } from "nanostores";
 
+const hexStringRegex = /(?:#?[0-9a-fA-F]{6}|#?[0-9a-fA-F]{3})/;
 export const textColor = atom('#000000');
 export const bgColor = atom('#ffffff');
+
+export const setTextColor = (newColor) => {
+	// check if it is an hex string
+	if (hexStringRegex.exec(newColor).length > 0)
+		textColor.set(newColor);
+}
+
+export const setBgColor = (newColor) => {
+	// check if it is an hex string
+	if (hexStringRegex.exec(newColor).length > 0)
+		bgColor.set(newColor);
+}
