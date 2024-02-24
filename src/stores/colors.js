cad61d2b415364d57da779c5fb2fd451774c2008
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-24 19:56:49 -0300

    Move logic to store.

--- src/stores/colors.js
+++ src/stores/colors.js
@@ -1,4 +1,6 @@
 import { atom } from "nanostores";
+import { calculateColorByCvd, rgb2hex } from "../utils";
+import { cvd } from "./cvd";
 
 const hexStringRegex = /(?:#?[0-9a-fA-F]{6}|#?[0-9a-fA-F]{3})/;
 
@@ -31,3 +33,26 @@ export const setCvdBgColor = (newColor) => {
 	if (hexStringRegex.exec(newColor).length > 0)
 		cvdBgColor.set(newColor);
 };
+
+
+// Set changes in color
+textColor.subscribe((v, ov) => {
+	setCvdTextColor(
+		rgb2hex(calculateColorByCvd(cvd.get().cvd, cvd.get().severity, v)),
+	);
+});
+
+bgColor.subscribe((v, ov) => {
+	setCvdBgColor(
+		rgb2hex(calculateColorByCvd(cvd.get().cvd, cvd.get().severity, v)),
+	);
+});
+
+cvd.subscribe((v, ov) => {
+	setCvdTextColor(
+		rgb2hex(calculateColorByCvd(v.cvd, v.severity, textColor.get())),
+	);
+	setCvdBgColor(
+		rgb2hex(calculateColorByCvd(v.cvd, v.severity, bgColor.get())),
+	);
+});
