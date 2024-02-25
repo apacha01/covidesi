41eb3bd1651fb5019d7ad143c33fcfc9f28f7499
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2024-02-25 10:40:26 -0300

    Refactor cvs colors calculation. Remove unnecesary state for cvd colors, calculate in situ when necessary.

--- src/stores/colors.js
+++ src/stores/colors.js
@@ -1,14 +1,9 @@
 import { atom } from "nanostores";
-import { calculateColorByCvd, rgb2hex } from "../utils";
-import { cvd } from "./cvd";
 
 const hexStringRegex = /(?:#?[0-9a-fA-F]{6}|#?[0-9a-fA-F]{3})/;
 
 export const textColor = atom('#000000');
 export const bgColor = atom('#ffffff');
-
-export const cvdTextColor = atom('#000000');
-export const cvdBgColor = atom('#ffffff');
 
 export const setTextColor = (newColor) => {
 	// check if it is an hex string
@@ -21,38 +16,3 @@ export const setBgColor = (newColor) => {
 	if (hexStringRegex.exec(newColor).length > 0)
 		bgColor.set(newColor);
 };
-
-export const setCvdTextColor = (newColor) => {
-	// check if it is an hex string
-	if (hexStringRegex.exec(newColor).length > 0)
-		cvdTextColor.set(newColor);
-};
-
-export const setCvdBgColor = (newColor) => {
-	// check if it is an hex string
-	if (hexStringRegex.exec(newColor).length > 0)
-		cvdBgColor.set(newColor);
-};
-
-
-// Set changes in color
-textColor.subscribe((v, ov) => {
-	setCvdTextColor(
-		rgb2hex(calculateColorByCvd(cvd.get().cvd, cvd.get().severity, v)),
-	);
-});
-
-bgColor.subscribe((v, ov) => {
-	setCvdBgColor(
-		rgb2hex(calculateColorByCvd(cvd.get().cvd, cvd.get().severity, v)),
-	);
-});
-
-cvd.subscribe((v, ov) => {
-	setCvdTextColor(
-		rgb2hex(calculateColorByCvd(v.cvd, v.severity, textColor.get())),
-	);
-	setCvdBgColor(
-		rgb2hex(calculateColorByCvd(v.cvd, v.severity, bgColor.get())),
-	);
-});
